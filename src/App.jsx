616b10068dcacf15d8f3f5f6cb434b1a700e69e6
ASWADHBS <bsaswadh@gmail.com>
2026-05-11 11:08:53 +0530

    Part 1: Add product display with Header, ProductList, and ProductCard components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import { products } from './data/products';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main-content">
+        <ProductList products={products} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
